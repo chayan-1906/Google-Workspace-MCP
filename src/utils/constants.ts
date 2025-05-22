534e349b86a1a8ad13960f3bb56dce6ea3453b81
Padmanabha Das <padmanabhadas9647@gmail.com>
2025-05-22 23:33:25 +0530

    added deleteRow and deleteColumn tools

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,6 +6,7 @@ const tools = {
     renameSpreadsheet: 'rename-spreadsheet',
 
     appendRow: 'append-row',
+    deleteRow: 'delete-row',
     clearRanges: 'clear-ranges',    // batchClear in remix
     updateRanges: 'update-ranges',  // update in remix
     getSheetContent: 'get-sheet-content',
@@ -16,14 +17,20 @@ const tools = {
     insertColumn: 'insert-column',
     deleteColumn: 'delete-column',
     addChart: 'add-chart',
-    sort: 'sort',
-    filter: 'filter',
-    functions: 'functions',
-    addConditionalFormatting: 'add-conditional-formatting',
-    getAllConditionalFormatting: 'get-all-conditional-formatting',
-    clearAllConditionalFormatting: 'clear-all-conditional-formatting',
-    freezeRowsColumns: 'freeze-rows-columns',
-    unfreezeRowsColumns: 'unfreeze-rows-columns',
+    sort: 'sort',                                                       // TODO - 11
+    filter: 'filter',                                                   // TODO - 12
+    functions: 'functions',                                             // TODO - 13
+    addConditionalFormatting: 'add-conditional-formatting',             // TODO - 1
+    getAllConditionalFormatting: 'get-all-conditional-formatting',      // TODO - 14
+    clearAllConditionalFormatting: 'clear-all-conditional-formatting',  // TODO - 4
+    freezeRowsColumns: 'freeze-rows-columns',                           // TODO - 9
+    unfreezeRowsColumns: 'unfreeze-rows-columns',                       // TODO - 10
+    customCellFormatting: 'custom-cell-formatting',                     // TODO - 2
+    duplicateSheet: 'duplicate-sheet',     // copySheet in remix        // TODO - 3
+    mergeCells: 'merge-cells',                                          // TODO - 7
+    unmergeCells: 'unmerge-cells',                                      // TODO - 8
+    setHeightWidth: 'set-height-width',                                 // TODO - 5
+    protectCells: 'protect-cells',                                      // TODO - 6
 }
 
 export {tools};
