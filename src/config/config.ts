f323a35bf3b9957292271d501f129019535a2314
Padmanabha Das <74731500+pdas9647@users.noreply.github.com>
2025-07-25 00:08:21 +0530

    Removed sensitive values from config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,7 @@
 export const PORT = 20251
-export const CLIENT_ID = "426503477689-h0fhctv6uhe2f11dv2nb12j40h56o9u9.apps.googleusercontent.com"
-export const CLIENT_SECRET = "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"
+export const DB_NAME = "google-workspace";
+export const CLIENT_ID = ""
+export const CLIENT_SECRET = ""
 export const REDIRECT_URI = `http://localhost:${PORT}/oauth2callback`
-export const MONGODB_URI = "mongodb+srv://chayancodes:el1YwX3YUr4dTke7@cluster0.jjt8clc.mongodb.net/google-workspace?retryWrites=true&w=majority"
-export const TOKEN_SECRET = "97f4a12e34cd8bf65a9e20b31d87fa09cb47de302f89c1a6e503da8421b7f650"
+export const MONGODB_URI = ""
+export const TOKEN_SECRET = ""
