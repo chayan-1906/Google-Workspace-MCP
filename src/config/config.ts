7fb7b7aae93d2ba510378a52f68c269e8fd94436
Padmanabha Das <82430454+chayan-1906@users.noreply.github.com>
2025-07-31 20:27:33 +0530

    Added config.ts template

---
+++ src/config/config.ts
@@ -0,0 +1,7 @@
+export const PORT = 20251
+export const DB_NAME = "google-workspace";
+export const CLIENT_ID = ""
+export const CLIENT_SECRET = ""
+export const REDIRECT_URI = `http://localhost:${PORT}/oauth2callback`
+export const MONGODB_URI = ""
+export const TOKEN_SECRET = ""
